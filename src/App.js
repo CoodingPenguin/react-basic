38431ec80e80b72e8abd2da2b3e86da2efb92c4e
CoodingPenguin <unodostre@naver.com>
2020-10-20 17:41:29 +0900

    Create Food component in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+// Food 컴포넌트 정의
+function Food({ fav }) {
+  return <h3>I like {fav}❤</h3>;
+}
 
 // App 컴포넌트 정의
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="MAKCHANG" />
+      <Food fav="JOKBAL" />
+      <Food fav="OIL PASTA" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato() {
-  return <h3>I love Potato🥔</h3>;
-}
-
-export default Potato;
