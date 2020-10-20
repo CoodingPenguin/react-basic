5197f8df1be0a9dde574bfd4bd7934f806ebaa02
CoodingPenguin <unodostre@naver.com>
2020-10-20 17:20:24 +0900

    docs: ✏️ add comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
+// App 컴포넌트 정의
 function App() {
-    return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// App 컴포넌트를 id가 root인 element 사이에 렌더링
+ReactDOM.render(<App />, document.getElementById("root"));
