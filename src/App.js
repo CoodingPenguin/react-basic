bbb7a3e83c04f601e1afd46490a882b027086353
CoodingPenguin <unodostre@naver.com>
2020-10-20 17:22:46 +0900

    Add Potato component to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Potato from "./Potato";
 
 // App 컴포넌트 정의
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // App 컴포넌트를 id가 root인 element 사이에 렌더링
+// 리액트는 최종으로 단 한 개의 컴포넌트만 렌더링
 ReactDOM.render(<App />, document.getElementById("root"));
