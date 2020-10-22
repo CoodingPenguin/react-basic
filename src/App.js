5e6e8d575816a81fcefbd3bf05b24091b9b97dfa
CoodingPenguin <unodostre@naver.com>
2020-10-22 17:21:20 +0900

    State and life-cycle functions

--- src/App.js
+++ src/App.js
@@ -1,69 +1,53 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-// Food 컴포넌트 정의
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I like {name}❤</h3>
-      <p>{rating} / 5</p>
-      <img src={picture} width="320px" alt={name} />
-    </div>
-  );
+class App extends React.Component {
+  // 1. 객체 생성 시 호출
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+
+  state = {
+    count: 0,
+  };
+
+  // count state를 1 증가
+  add = () => {
+    console.log("add");
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+
+  // count state를 1 감소
+  minus = () => {
+    console.log("minus");
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  // 3. 렌더링이 끝난 직후 호출
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  // 4. state가 변경되고 렌더링이 끝난 후 호출
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  // 5. 컴포넌트가 죽으면 호출
+  componentWillUnmount() {
+    console.log("Goodbye~");
+  }
+
+  // 2. 화면에 JSX를 렌더링
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-// 서버 데이터
-const foodLikes = [
-  {
-    id: 1,
-    name: "makchang",
-    image:
-      "https://lh3.googleusercontent.com/proxy/KozhupiTvsaxP8QCHYVkS8ykYEMUisSnQdQNpjo-aSRYjw0SNTg8bxytkpWX6BtltP8DFDAX2HoiJDNzMdvCKVprUKSeoZ1LeRIUuyd_RnoUXkQ3mPs",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "jokbal",
-    image: "https://image.auction.co.kr/itemimage/1d/4c/66/1d4c66f2a6.jpg",
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: "oil pasta",
-    image:
-      "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/618254_1474937856744072.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
-    rating: 3,
-  },
-  {
-    id: 4,
-    name: "cheese burger",
-    image: "https://image.auction.co.kr/itemimage/19/d2/93/19d2932eb6.jpg",
-    rating: 3,
-  },
-];
-
-// App 컴포넌트 정의
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodLikes.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-// Food 컴포넌트 props 검사
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
 export default App;
