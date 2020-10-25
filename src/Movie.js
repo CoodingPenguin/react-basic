23dcc4de787ad9c7ee20485802d7c31ababf59d8
CoodingPenguin <unodostre@naver.com>
2020-10-25 18:26:17 +0900

    Change class to className

--- src/Movie.js
+++ src/Movie.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // state가 불필요하므로 함수형 컴포넌트로 작성
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
         <h5 ckass="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -21,6 +21,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
