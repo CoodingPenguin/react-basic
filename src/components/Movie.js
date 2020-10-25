0d8c41f67019ed78f927ff3dd57b5f41659aa78d
CoodingPenguin <unodostre@naver.com>
2020-10-25 19:09:01 +0900

    Add link property to movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 // state가 불필요하므로 함수형 컴포넌트로 작성
 function Movie({ title, year, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 ckass="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => {
-            return (
-              <li key={index} className="movie__genre">
-                {genre}
-              </li>
-            );
-          })}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...</p>
-      </div>
+      <Link
+        to={{
+          pathname: "/movie-detail",
+          state: { year, title, summary, poster, genres },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 ckass="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => {
+              return (
+                <li key={index} className="movie__genre">
+                  {genre}
+                </li>
+              );
+            })}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
   );
 }
